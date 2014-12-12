aacb95964040e5d2bb61ae4265a99074da71a68e
astrojohnlewis <astrojohnlewis@users.noreply.github.com>
2014-12-11 19:51:14 -0500

    Update Photometry.py

--- Photometry.py
+++ Photometry.py
@@ -1,24 +1,24 @@
 def photometry(i,ap):
-  import numpy as np
+  	import numpy as np
 	import astropy.io.fits as fits
 	import photutils
 	h= i.rstrip(".fits")
   #Read in fits file
-  hdulist=fits.open(i)
-  data=hdulist[0]
-  file=np.array(data.data)
+	hdulist=fits.open(i)
+	data=hdulist[0]
+  	file=np.array(data.data)
 	hdulist.close(i)
  
   #Setting detection limit to Standard Deviation
-  flat_file=file.ravel()
-  sigma=np.std(flat_file)
-  floor=np.median(flat_file)
-  limit=floor+(1*sigma)
+  	flat_file=file.ravel()
+  	sigma=np.std(flat_file)
+  	floor=np.median(flat_file)
+  	limit=floor+(1*sigma)
 
   #Perform photometry
-  coords=photutils.find_peaks(file,limit)  #goes (y,x)
-  apertures=photutils.CircularAperture(coords, r=ap)
-  from photutils import aperture_photometry
-  phot_table=aperture_photometry(file,apertures)
+  	coords=photutils.find_peaks(file,limit)  #goes (y,x)
+  	apertures=photutils.CircularAperture(coords, r=ap)
+  	from photutils import aperture_photometry
+  	phot_table=aperture_photometry(file,apertures)
 	from astropy.table import Table
 	phot_table.write(h+"_table", format="ipac")
