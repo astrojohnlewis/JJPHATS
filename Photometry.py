59db092d2462e5573625b55635b7ba3a2f5f7648
John <astrojohnlewis@users.noreply.github.com>
2015-01-12 19:18:53 -0500

    Update Photometry.py

--- Photometry.py
+++ Photometry.py
@@ -38,7 +38,7 @@ def photometry(i,ap,sig):
   	coords=photutils.find_peaks(file,limit)  #returns (y,x) coords of peaks
   	apertures=photutils.CircularAperture(coords, r=ap)  #puts apertures around peaks
   	from photutils import aperture_photometry
-  	print 'Doing Photometry Now..."
+  	print "Doing Photometry Now..."
   	phot_table=aperture_photometry(file,apertures) #do photometry
 	from astropy.table import Table
 	print "Writing IPAC table (readable by DS9)"
@@ -52,6 +52,7 @@ def photometry(i,ap,sig):
 	plt.plot(tab['ycenter'],tab['xcenter'],'o',mfc='None',mec='b')
 	plt.xlim(0,file.shape[1])
 	plt.ylim(0,file.shape[0])
+	#apertures.plot(color='blue', lw=1.5, alpha=0.5))
     	plt.show()
 	print "Returning tuple (phot_table,apertures) ..."
 	return phot_table,apertures
