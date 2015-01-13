349d92ee1952705df2362f7b14e2df08dd11f461
John <astrojohnlewis@users.noreply.github.com>
2015-01-12 19:34:25 -0500

    Update and include small fixes

--- Photometry.py
+++ Photometry.py
@@ -5,55 +5,54 @@ import photometry
 photom = photometry(filename, aperture, sigam_level)
 """
 
-def photometry(i,ap,sig):
+def photometry(filename,ap,signf):
 	"""function inputs
-	i - file name
+	filename - :)
 	ap - aperture size, i.e. average radius
 	of star in pixels
-	sig - number of sigma for detection (rec. 1000)
+	signf - detection significance (signf * sigma)
 	"""
-  	import numpy as np
+	import numpy as np
 	import astropy.io.fits as fits
 	import photutils
-	h= i.rstrip(".fits") 
-  #Read in fits file
-  	print "Reading in file ..."
-	hdulist=fits.open(i) 
-	data=hdulist[0]
-  	file=np.array(data.data)
-	hdulist.close(i)
- 
-  #Setting detection limit to Standard Deviation
-  	print "Setting limits ..."
-  	flat_file=file.ravel() #flatten to 1D
-  	sigma=np.std(flat_file) # get standard deviation
-  	# probably need to include sigma clipping
-  	# sigma is likely high
-  	floor=np.median(flat_file)
-  	# floor is likely high
-  	limit=floor+(sig*sigma)
-
-  #Perform photometry
-  	print "Finding peaks ..."
-  	coords=photutils.find_peaks(file,limit)  #returns (y,x) coords of peaks
-  	apertures=photutils.CircularAperture(coords, r=ap)  #puts apertures around peaks
-  	from photutils import aperture_photometry
-  	print "Doing Photometry Now..."
-  	phot_table=aperture_photometry(file,apertures) #do photometry
-	from astropy.table import Table
-	print "Writing IPAC table (readable by DS9)"
-	phot_table.write(h+"_table", format="ipac")
+	
+	sourcename= filename.rstrip(".fits")
+	#Read in fits file
+	print "Reading in file ..."
+	hdulist = fits.open(filename)
+	image = hdulist[0].data
+	hdulist.close()
+	
+	#Setting detection limit to Standard Deviation
+	print "Setting limits ..."
+	sigma = np.std(image) # get standard deviation
+	# probably need to include sigma clipping
+	# sigma is likely high
+	noisefloor = np.median(image)
+	# floor is likely high
+	limit = noisefloor + (signf * sigma)
+	
+	#Perform photometry
+	print "Finding peaks ..."
+	coords=photutils.find_peaks(image,limit)  #returns coords of peaks
+	apertures=photutils.CircularAperture(coords, r = ap)  #puts apertures around peaks
+	print "Doing Photometry Now..."
+	phot_table=photutils.aperture_photometry(image, apertures) #do photometry
+	
+	#print "Writing IPAC table"
+	#phot_table.write(sourcename+"_table", format="ipac")
 	print "Displaying Plot of stars and images"
-	print "Either save or close image to continue"
 	import matplotlib.pylab as plt
 	#Display apertures over image
 	import matplotlib.pylab as plt
-	plt.imshow(file,cmap='gray_r')
+	#plt.imshow(image, cmap='gray_r', origin='lower')
+	#apertures.plot(color='blue', lw=1.5, alpha=0.5)
+	plt.imshow(image,cmap='gray_r')
 	plt.plot(tab['ycenter'],tab['xcenter'],'o',mfc='None',mec='b')
 	plt.xlim(0,file.shape[1])
 	plt.ylim(0,file.shape[0])
-	#apertures.plot(color='blue', lw=1.5, alpha=0.5))
-    	plt.show()
+	plt.show()
+	
 	print "Returning tuple (phot_table,apertures) ..."
 	return phot_table,apertures
 	
